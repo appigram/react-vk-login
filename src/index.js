73b0889f0d70b38e5cf7f7d22ff28cb071c42252
Eugene Sysmanov <appigram@gmail.com>
2017-08-05 23:28:00 +0300

    deps update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from 'react'
-import autobind from 'autobind-decorator'
-import getURL from './getURL'
-import getQueryParameter from './getQueryParameter'
-import reset from './reset'
+import React from 'react';
+import getURL from './getURL';
+import getQueryParameter from './getQueryParameter';
+import reset from './reset';
 /* global localStorage */
 
 export default class VKLogin extends React.Component {
@@ -12,42 +11,40 @@ export default class VKLogin extends React.Component {
     callback: React.PropTypes.func.isRequired,
     className: React.PropTypes.string,
     text: React.PropTypes.node,
-    scope: React.PropTypes.arrayOf(React.PropTypes.string)
+    scope: React.PropTypes.arrayOf(React.PropTypes.string),
   }
 
-  componentDidMount () {
-    this.restart()
+  componentDidMount() {
+    this.restart();
   }
 
-  @autobind
-  start () {
-    const state = Random.id()
-    const clientId = this.props.clientId
-    const scope = this.props.scope
-    localStorage.vkReactLogin = state
-    localStorage.vkReactLoginRedirectUri = window.location.href
-    window.location.href = getURL({ clientId, state, scope })
+  start = () => {
+    const state = Math.random().toString(36).substring(7);
+    const clientId = this.props.clientId;
+    const scope = this.props.scope;
+    localStorage.vkReactLogin = state;
+    localStorage.vkReactLoginRedirectUri = window.location.href;
+    window.location.href = getURL({ clientId, state, scope });
   }
 
-  @autobind
-  restart () {
-    const state = localStorage.vkReactLogin
-    const redirectUri = localStorage.vkReactLoginRedirectUri
-    if (!redirectUri) return
-    if (!state) return
-    if (state !== getQueryParameter('state')) return
-    if (!getQueryParameter('code')) return
-    const code = getQueryParameter('code')
-    reset()
-    this.props.callback({code, redirectUri})
+  restart = () => {
+    const state = localStorage.vkReactLogin;
+    const redirectUri = localStorage.vkReactLoginRedirectUri;
+    if (!redirectUri) return;
+    if (!state) return;
+    if (state !== getQueryParameter('state')) return;
+    if (!getQueryParameter('code')) return;
+    const code = getQueryParameter('code');
+    reset();
+    this.props.callback({ code, redirectUri });
   }
 
-  render () {
+  render() {
     return (
       <button className={this.props.className} onClick={this.start}>
         {this.props.text}
       </button>
-    )
+    );
   }
 
 }
